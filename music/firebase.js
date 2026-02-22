2d0ce4d9605847f9756cbedeb8314bd88af12a70
Davemiracle01 <davidmiracle514@gmail.com>
2026-02-22 13:36:16 +0300

    Add files via upload

---
+++ music/firebase.js
@@ -0,0 +1,19 @@
+/**
+ * ╔══════════════════════════════════════════════════════════╗
+ * ║   A.S.T.A.™ FIREBASE CONFIG                             ║
+ * ║   Edit DASHBOARD_PASSWORD to change your password       ║
+ * ╚══════════════════════════════════════════════════════════╝
+ */
+
+window.FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyCB8u4AklnRXPr9LqJxuipc-7sxFfyBojc",
+  authDomain:        "invest-hub-9dacb.firebaseapp.com",
+  databaseURL:       "https://invest-hub-9dacb-default-rtdb.firebaseio.com",
+  projectId:         "invest-hub-9dacb",
+  storageBucket:     "invest-hub-9dacb.firebasestorage.app",
+  messagingSenderId: "54499046393",
+  appId:             "1:54499046393:web:d6596c302fa173eb7a0328",
+};
+
+window.DASHBOARD_PASSWORD = "david2025";
+window.DASHBOARD_TITLE    = "A.S.T.A.™ COMMAND CENTRE";
